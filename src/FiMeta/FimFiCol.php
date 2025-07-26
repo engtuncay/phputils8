e1f040edf5956e806abc129d6792811a0a6b6a8e
TuncayHp2 <tunc270@gmail.com>
2025-07-26 22:08:18 +0300

    up

---
+++ src/FiMeta/FimFiCol.php
@@ -0,0 +1,149 @@
+<?php
+
+namespace Engtuncay\Phputils8\FiMeta;
+
+// Codegen v2
+
+use Engtuncay\Phputils8\Meta\FiMeta;
+use Engtuncay\Phputils8\Meta\FmtList;
+
+class FimFiCol
+{
+
+
+  // public static function genTableCols() : FicList {
+  //   $ficList = new FicList();
+
+
+
+  //   return $ficList;
+  // }
+
+  // public static function genTableColsTrans() : FicList {
+  //   $ficList = new FicList();
+
+
+
+  //   return $ficList;
+  // }
+
+  public static function ofcTxDesc(): FiMeta
+  {
+    $fiMeta = new FiMeta("ofcTxDesc");
+
+    return $fiMeta;
+  }
+
+  public static function ofcTxPrefix(): FiMeta
+  {
+    $fiMeta = new FiMeta("ofcTxPrefix");
+
+    return $fiMeta;
+  }
+
+  public static function ofcTxEntityName(): FiMeta
+  {
+    $fiMeta = new FiMeta("ofcTxEntityName");
+
+    return $fiMeta;
+  }
+
+  public static function ofcTxFieldName(): FiMeta
+  {
+    $fiMeta = new FiMeta("ofcTxFieldName");
+
+    return $fiMeta;
+  }
+
+  public static function ofcTxFieldType(): FiMeta
+  {
+    $fiMeta = new FiMeta("ofcTxFieldType");
+
+    return $fiMeta;
+  }
+
+  public static function ofcTxHeader(): FiMeta
+  {
+    $fiMeta = new FiMeta("ofcTxHeader");
+
+    return $fiMeta;
+  }
+
+  public static function ofcTxDbField(): FiMeta
+  {
+    $fiMeta = new FiMeta("ofcTxDbField");
+
+    return $fiMeta;
+  }
+
+  public static function ofcTxRefField(): FiMeta
+  {
+    $fiMeta = new FiMeta("ofcTxRefField");
+
+    return $fiMeta;
+  }
+
+  public static function ofcLnLength(): FiMeta
+  {
+    $fiMeta = new FiMeta("ofcLnLength");
+
+    return $fiMeta;
+  }
+
+  public static function ofcLnPrecision(): FiMeta
+  {
+    $fiMeta = new FiMeta("ofcLnPrecision");
+
+    return $fiMeta;
+  }
+
+  public static function ofcLnScale(): FiMeta
+  {
+    $fiMeta = new FiMeta("ofcLnScale");
+
+    return $fiMeta;
+  }
+
+  public static function ofcBoNullable(): FiMeta
+  {
+    $fiMeta = new FiMeta("ofcBoNullable");
+
+    return $fiMeta;
+  }
+
+  public static function ofcTxIdType(): FiMeta
+  {
+    $fiMeta = new FiMeta("ofcTxIdType");
+
+    return $fiMeta;
+  }
+
+  public static function ofcBoTransient(): FiMeta
+  {
+    $fiMeta = new FiMeta("ofcBoTransient");
+
+    return $fiMeta;
+  }
+
+  public static function ofcBoUnique(): FiMeta
+  {
+    $fiMeta = new FiMeta("ofcBoUnique");
+
+    return $fiMeta;
+  }
+
+  public static function ofcBoUniqGro1(): FiMeta
+  {
+    $fiMeta = new FiMeta("ofcBoUniqGro1");
+
+    return $fiMeta;
+  }
+
+  public static function ofcTxDefValue(): FiMeta
+  {
+    $fiMeta = new FiMeta("ofcTxDefValue");
+
+    return $fiMeta;
+  }
+
+}
