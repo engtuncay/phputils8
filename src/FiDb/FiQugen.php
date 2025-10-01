a4b11d0c3e0800c84caa7497193e7aa1b50639e9
Tuncay <tunc270@gmail.com>
2025-10-01 20:37:00 +0300

    up

--- src/FiDb/FiQugen.php
+++ src/FiDb/FiQugen.php
@@ -17,15 +17,23 @@ class FiQugen
   {
     $sbSql = new FiStrbui();
 
+    $arrFieldId = [];
+
+    foreach ($fdrFields as $refFields) {
+      $arrFieldId[] = $refFields['Field'];
+    }
+
+    log_message('info', 'Mevcut alanlar: ' . print_r($arrFieldId, true));
+
     /** 
      *  @var FkbList $fkbFields 
      *  @var FiKeybean $fkb
      */
     foreach ($fkbFields as $fkb) {
-      if (!in_array($fkb->getOfcTxFn(), $fdrFields)) {
+      if (!in_array($fkb->getOfcTxFn(), $arrFieldId)) {
         // Field needs to be added
         $sbSql->append("ALTER TABLE payments_log ADD COLUMN {$fkb->getOfcTxFn()} {$fkb->getValueByFiMeta(FimFiCol::ofcTxFieldType())};");
-        $sbSql->append("<br/>");
+        $sbSql->append("\n");
         // Execute the SQL
       }
     }
