460e6f9b17899a5f9e6f6276b870b0d842ac5c5b
TuncayHp2 <tunc270@gmail.com>
2026-02-09 14:32:35 +0300

    up

--- src/FiDbs/FiConnConfig.php
+++ src/FiDbs/FiConnConfig.php
@@ -4,70 +4,96 @@ namespace Engtuncay\Phputils8\FiDbs;
 
 class FiConnConfig
 {
-    private ?string $txServer = null;
-    private ?string $txDatabase = null;
-    private ?string $txUsername = null;
-    private ?string $txPass = null;
+  private ?string $txServer = null;
+  private ?string $txDatabase = null;
+  private ?string $txUsername = null;
+  private ?string $txPass = null;
 
-    /**
-     * @return mixed
-     */
-    public function getTxServer()
-    {
-        return $this->txServer;
-    }
+  private ?string $txDbType = null;
 
-    /**
-     * @param mixed $hostname
-     */
-    public function setTxServer($hostname): void
-    {
-        $this->txServer = $hostname;
-    }
+  /**
+   * @return mixed
+   */
+  public function getTxServer()
+  {
+    return $this->txServer;
+  }
 
-    /**
-     * @return mixed
-     */
-    public function getTxDatabase()
-    {
-        return $this->txDatabase;
-    }
+  /**
+   * @param mixed $hostname
+   */
+  public function setTxServer($hostname): void
+  {
+    $this->txServer = $hostname;
+  }
 
-    /**
-     * @param mixed $dbname
-     */
-    public function setTxDatabase($dbname): void
-    {
-        $this->txDatabase = $dbname;
-    }
+  /**
+   * @return mixed
+   */
+  public function getTxDatabase()
+  {
+    return $this->txDatabase;
+  }
 
-    /**
-     * @return mixed
-     */
-    public function getTxUsername(): ?string
-    {
-        return $this->txUsername;
-    }
+  /**
+   * @param mixed $dbname
+   */
+  public function setTxDatabase($dbname): void
+  {
+    $this->txDatabase = $dbname;
+  }
 
-    /**
-     * @param mixed $txUsername
-     */
-    public function setTxUsername($txUsername): void
-    {
-        $this->txUsername = $txUsername;
-    }
+  /**
+   * @return mixed
+   */
+  public function getTxUsername(): ?string
+  {
+    return $this->txUsername;
+  }
 
-    public function getTxPass(): ?string
-    {
-        return $this->txPass;
-    }
+  /**
+   * @param mixed $txUsername
+   */
+  public function setTxUsername($txUsername): void
+  {
+    $this->txUsername = $txUsername;
+  }
 
-    /**
-     * @param mixed $txPass
-     */
-    public function setTxPass($txPass): void
-    {
-        $this->txPass = $txPass;
-    }
+  public function getTxPass(): ?string
+  {
+    return $this->txPass;
+  }
 
+  /**
+   * @param mixed $txPass
+   */
+  public function setTxPass($txPass): void
+  {
+    $this->txPass = $txPass;
+  }
+
+  /**
+   * Get the value of txDbType
+   *
+   * @return ?string
+   */
+  public function getTxDbType(): ?string
+  {
+    return $this->txDbType;
+  }
+
+  /**
+   * Set the value of txDbType
+   *
+   * @param ?string $txDbType
+   *
+   * @return self
+   */
+  public function setTxDbType(?string $txDbType): self
+  {
+    $this->txDbType = $txDbType;
+
+    return $this;
+  }
+  
 }
