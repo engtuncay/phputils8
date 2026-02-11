7ffe4ad92b77eff5e271406ce3c9bc28be9381b0
TuncayHp2 <tunc270@gmail.com>
2026-02-11 14:40:43 +0300

    up

--- src/FiMetas/FimFiCol.php
+++ src/FiMetas/FimFiCol.php
@@ -3,132 +3,130 @@
 namespace Engtuncay\Phputils8\FiMetas;
 
 // FiMeta Class Generation (By Dml) - v0.3 
-      
+
 use Engtuncay\Phputils8\FiDtos\FiMeta;
 use Engtuncay\Phputils8\FiDtos\FmtList;
 
-class FimFiCol {
+class FimFiCol
+{
 
 
-public static function fcTxEntityName() : FiMeta
-{ 
-  $fiMeta = new FiMeta("fcTxEntityName");
+  public static function fcTxEntityName(): FiMeta
+  {
+    $fiMeta = new FiMeta("fcTxEntityName");
 
-  return $fiMeta;
-}
+    return $fiMeta;
+  }
 
-public static function fcTxFieldType() : FiMeta
-{ 
-  $fiMeta = new FiMeta("fcTxFieldType");
+  public static function fcTxFieldType(): FiMeta
+  {
+    $fiMeta = new FiMeta("fcTxFieldType");
 
-  return $fiMeta;
-}
+    return $fiMeta;
+  }
 
-public static function fcTxFieldName() : FiMeta
-{ 
-  $fiMeta = new FiMeta("fcTxFieldName");
+  public static function fcTxFieldName(): FiMeta
+  {
+    $fiMeta = new FiMeta("fcTxFieldName");
 
-  return $fiMeta;
-}
+    return $fiMeta;
+  }
 
-public static function fcTxHeader() : FiMeta
-{ 
-  $fiMeta = new FiMeta("fcTxHeader");
+  public static function fcTxHeader(): FiMeta
+  {
+    $fiMeta = new FiMeta("fcTxHeader");
 
-  return $fiMeta;
-}
+    return $fiMeta;
+  }
 
-public static function fcTxFieldDesc() : FiMeta
-{ 
-  $fiMeta = new FiMeta("fcTxFieldDesc");
+  public static function fcTxFieldDesc(): FiMeta
+  {
+    $fiMeta = new FiMeta("fcTxFieldDesc");
 
-  return $fiMeta;
-}
+    return $fiMeta;
+  }
 
-public static function fcTxIdType() : FiMeta
-{ 
-  $fiMeta = new FiMeta("fcTxIdType");
+  public static function fcTxIdType(): FiMeta
+  {
+    $fiMeta = new FiMeta("fcTxIdType");
 
-  return $fiMeta;
-}
+    return $fiMeta;
+  }
 
-public static function fcBoTransient() : FiMeta
-{ 
-  $fiMeta = new FiMeta("fcBoTransient");
+  public static function fcBoTransient(): FiMeta
+  {
+    $fiMeta = new FiMeta("fcBoTransient");
 
-  return $fiMeta;
-}
+    return $fiMeta;
+  }
 
-public static function fcLnLength() : FiMeta
-{ 
-  $fiMeta = new FiMeta("fcLnLength");
+  public static function fcLnLength(): FiMeta
+  {
+    $fiMeta = new FiMeta("fcLnLength");
 
-  return $fiMeta;
-}
+    return $fiMeta;
+  }
 
-public static function fcBoNullable() : FiMeta
-{ 
-  $fiMeta = new FiMeta("fcBoNullable");
+  public static function fcBoNullable(): FiMeta
+  {
+    $fiMeta = new FiMeta("fcBoNullable");
 
-  return $fiMeta;
-}
+    return $fiMeta;
+  }
 
-public static function fcLnPrecision() : FiMeta
-{ 
-  $fiMeta = new FiMeta("fcLnPrecision");
+  public static function fcLnPrecision(): FiMeta
+  {
+    $fiMeta = new FiMeta("fcLnPrecision");
 
-  return $fiMeta;
-}
+    return $fiMeta;
+  }
 
-public static function fcLnScale() : FiMeta
-{ 
-  $fiMeta = new FiMeta("fcLnScale");
+  public static function fcLnScale(): FiMeta
+  {
+    $fiMeta = new FiMeta("fcLnScale");
 
-  return $fiMeta;
-}
+    return $fiMeta;
+  }
 
-public static function fcBoUnique() : FiMeta
-{ 
-  $fiMeta = new FiMeta("fcBoUnique");
+  public static function fcBoUnique(): FiMeta
+  {
+    $fiMeta = new FiMeta("fcBoUnique");
 
-  return $fiMeta;
-}
+    return $fiMeta;
+  }
 
-public static function fcBoUniqGro1() : FiMeta
-{ 
-  $fiMeta = new FiMeta("fcBoUniqGro1");
+  public static function fcBoUniqGro1(): FiMeta
+  {
+    $fiMeta = new FiMeta("fcBoUniqGro1");
 
-  return $fiMeta;
-}
+    return $fiMeta;
+  }
 
-public static function fcTxDefValue() : FiMeta
-{ 
-  $fiMeta = new FiMeta("fcTxDefValue");
+  public static function fcTxDefValue(): FiMeta
+  {
+    $fiMeta = new FiMeta("fcTxDefValue");
 
-  return $fiMeta;
-}
+    return $fiMeta;
+  }
 
-public static function fcTxCollation() : FiMeta
-{ 
-  $fiMeta = new FiMeta("fcTxCollation");
+  public static function fcTxCollation(): FiMeta
+  {
+    $fiMeta = new FiMeta("fcTxCollation");
 
-  return $fiMeta;
-}
+    return $fiMeta;
+  }
 
-public static function fcTxTypeName() : FiMeta
-{ 
-  $fiMeta = new FiMeta("fcTxTypeName");
+  public static function fcTxTypeName(): FiMeta
+  {
+    $fiMeta = new FiMeta("fcTxTypeName");
 
-  return $fiMeta;
-}
+    return $fiMeta;
+  }
 
-public static function fcBoFilterLike() : FiMeta
-{ 
-  $fiMeta = new FiMeta("fcBoFilterLike");
+  public static function fcBoFilterLike(): FiMeta
+  {
+    $fiMeta = new FiMeta("fcBoFilterLike");
 
-  return $fiMeta;
-}
-
-
-
+    return $fiMeta;
+  }
 }
