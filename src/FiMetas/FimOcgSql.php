d808e50864f145538f7d2ac1d65ed1b1a93dfd70
TuncayHp2 <tunc270@gmail.com>
2026-02-22 14:13:16 +0300

    up

---
+++ src/FiMetas/FimOcgSql.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Engtuncay\Phputils8\FiMetas;
+
+// FiMeta Class Generation (By Dml) - v0.4 
+
+use Engtuncay\Phputils8\FiDtos\FiMeta;
+
+
+class FimOcgSql
+{
+
+
+  public static function sfTableName(): FiMeta
+  {
+    $fiMeta = new FiMeta("sfTableName");
+
+    return $fiMeta;
+  }
+
+  public static function sfTableFields(): FiMeta
+  {
+    $fiMeta = new FiMeta("sfTableFields");
+
+    return $fiMeta;
+  }
+
+  public static function sfCsvFields(): FiMeta
+  {
+    $fiMeta = new FiMeta("sfCsvFields");
+
+    return $fiMeta;
+  }
+
+  public static function sfTxWhere(): FiMeta
+  {
+    $fiMeta = new FiMeta("sfTxWhere");
+
+    return $fiMeta;
+  }
+}
